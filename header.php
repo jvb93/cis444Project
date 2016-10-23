dfef68292e5f8bfe2a782ad039d6fb8085215204
Justin Van Bibber <Justin Van Bibber>
2016-10-22 20:07:48 -0700

    template changes
    found a starting point. stuck with it. need ot break it out into other files

--- header.php
+++ header.php
@@ -5,11 +5,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title>Bootstrap 101 Template</title>
+    <title>Cougar eats</title>
 
     <!-- Bootstrap -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
-
+    <link href="css/site.css" rel="stylesheet">
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
@@ -18,6 +18,6 @@
     <![endif]-->
   </head>
   <body>
-    
+
 
   
--- mainpage.php
+++ mainpage.php
@@ -1,3 +1,65 @@
 <?php include 'header.php';?>
-<h1>Hello, World! </h1>
-<?php include 'footer.php';?>
+<div class="navbar navbar-inverse navbar-fixed-top">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">Brand</a>
+    </div>
+    <div class="collapse navbar-collapse">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+    </div><!--/.nav-collapse -->
+</div><!--/.navbar -->
+
+<div class="row-offcanvas row-offcanvas-left">
+  <div id="sidebar" class="sidebar-offcanvas">
+      <div class="col-md-12">
+        <h3>Sidebar (fixed)</h3>
+        <ul class="nav nav-pills nav-stacked">
+          <li class="active"><a href="#">Section</a></li>
+          <li><a href="#">Section</a></li>
+          <li><a href="#">Section</a></li>
+          <li><a href="#">Section</a></li>
+          <li><a href="#">Section</a></li>
+          <li><a href="#">Section</a></li>
+          <li><a href="#">Section</a></li>
+          <li><a href="#">Section</a></li>
+          <li><a href="#">Section</a></li>
+          <li><a href="#">Section</a></li>
+        </ul>
+      </div>
+  </div>
+  <div id="main">
+      <div class="col-md-12">
+      	  <p class="visible-xs">
+            <button type="button" class="btn btn-primary btn-xs" data-toggle="offcanvas"><i class="glyphicon glyphicon-chevron-left"></i></button>
+          </p>
+          <h2>Fixed + Fluid Bootstrap Template with Off-canvas Sidebar</h2>
+          <div class="row">
+              <div class="col-md-12"><div class="well"><p>Shrink the browser width to make the sidebar collapse off canvase.</p></div></div>
+          </div>
+          <div class="row">
+              <div class="col-md-4"><div class="well"><p>4 cols</p></div></div>
+              <div class="col-md-4"><div class="well"><p>4 cols</p></div></div>
+              <div class="col-md-4"><div class="well"><p>4 cols</p></div></div>
+          </div>
+          <div class="row">
+              <div class="col-lg-6 col-sm-6"><div class="well"><p>6 cols, 6 small cols</p></div></div>
+              <div class="col-lg-6 col-sm-6"><div class="well"><p>6 cols, 6 small cols</p></div></div>
+          </div>
+          <div class="row">
+              <div class="col-lg-4 col-sm-6"><div class="well">4 cols, 6 small cols</div></div>
+              <div class="col-lg-4 col-sm-6"><div class="well">4 cols, 6 small cols</div></div>
+              <div class="col-lg-4 col-sm-12"><div class="well">4 cols, 12 small cols</div></div>
+          </div>
+      </div>
+  </div>
+</div><!--/row-offcanvas -->
+
+<?php include 'footer.php'; ?>
