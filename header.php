f7db9ca461ef7ca3338ba846cc38469550b07697
Justin Van Bibber <justin@enteracloud.com>
2016-10-23 15:05:09 -0700

    moved the sidebar
    sidebar is now a part of header.php as it should be

--- header.php
+++ header.php
@@ -18,6 +18,42 @@
     <![endif]-->
   </head>
   <body>
+    <div class="navbar navbar-inverse navbar-fixed-top">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">Brand</a>
+    </div>
+    <div class="collapse navbar-collapse">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+    </div><!--/.nav-collapse -->
+</div><!--/.navbar -->
+
+<div class="row-offcanvas row-offcanvas-left">
+  <div id="sidebar" class="sidebar-offcanvas">
+      <div class="col-md-12">
+        <h3>Sidebar (fixed)</h3>
+        <ul class="nav nav-pills nav-stacked">
+          <li class="active"><a href="#">Section</a></li>
+          <li><a href="#">Section</a></li>
+          <li><a href="#">Section</a></li>
+          <li><a href="#">Section</a></li>
+          <li><a href="#">Section</a></li>
+          <li><a href="#">Section</a></li>
+          <li><a href="#">Section</a></li>
+          <li><a href="#">Section</a></li>
+          <li><a href="#">Section</a></li>
+          <li><a href="#">Section</a></li>
+        </ul>
+      </div>
+  </div>
 
 
   
---
+++ index.php
@@ -0,0 +1,30 @@
+<?php include 'header.php';?>
+
+  <div id="main">
+      <div class="col-md-12">
+      	  <p class="visible-xs">
+            <button type="button" class="btn btn-primary btn-xs" data-toggle="offcanvas"><i class="glyphicon glyphicon-chevron-left"></i></button>
+          </p>
+          <h2>Fixed + Fluid Bootstrap Template with Off-canvas Sidebar</h2>
+          <div class="row">
+              <div class="col-md-12"><div class="well"><p>Shrink the browser width to make the sidebar collapse off canvase.</p></div></div>
+          </div>
+          <div class="row">
+              <div class="col-md-4"><div class="well"><p>4 cols</p></div></div>
+              <div class="col-md-4"><div class="well"><p>4 cols</p></div></div>
+              <div class="col-md-4"><div class="well"><p>4 cols</p></div></div>
+          </div>
+          <div class="row">
+              <div class="col-lg-6 col-sm-6"><div class="well"><p>6 cols, 6 small cols</p></div></div>
+              <div class="col-lg-6 col-sm-6"><div class="well"><p>6 cols, 6 small cols</p></div></div>
+          </div>
+          <div class="row">
+              <div class="col-lg-4 col-sm-6"><div class="well">4 cols, 6 small cols</div></div>
+              <div class="col-lg-4 col-sm-6"><div class="well">4 cols, 6 small cols</div></div>
+              <div class="col-lg-4 col-sm-12"><div class="well">4 cols, 12 small cols</div></div>
+          </div>
+      </div>
+  </div>
+</div><!--/row-offcanvas -->
+
+<?php include 'footer.php'; ?>
