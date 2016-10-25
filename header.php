534ebaaf152b900511c0a687886b78f1da486964
Shane <shane.s.sharareh@gmail.com>
2016-10-25 01:46:35 -0700

    Uploaded profile.php
    I created a "view profile" page as well, the display picture goes out of
    place once it gets wider...

--- header.php
+++ header.php
@@ -31,10 +31,10 @@
       </div>
       <div id="navbar" class="collapse navbar-collapse">
         <ul class="nav navbar-nav">
-          <li class="active"><a href="#">Home</a></li>
+          <li class="active"><a href="home.php">Home</a></li>
           <li><a href="#about">About</a></li>
           <li><a href="#contact">Contact</a></li>
-          <li><a href="login.php">Login</a></li> 
+          <li><a href="login.php">Login</a></li>
         </ul>
         <div class="col-sm-3 col-md-3 navbar-right">
         <form class="navbar-form" role="search">
---
+++ profile.php
@@ -0,0 +1,82 @@
+<?php include 'header.php';?>
+<div class="container">
+      <div class="row">
+      <div class="col-md-5  toppad  pull-right col-md-offset-3 ">
+           <a href="edit-profile.php" >Edit Profile</a>
+
+        <a href="login.php" >Logout</a>
+       <br>
+
+      </div>
+        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xs-offset-0 col-sm-offset-0 col-md-offset-3 col-lg-offset-3 toppad" >
+
+
+          <div class="panel panel-info">
+            <div class="panel-heading">
+              <h3 class="panel-title Personal-info-header">John Smith</h3>
+            </div>
+            <div class="panel-body">
+              <div class="row">
+                <div class="col-md-3 col-lg-3 " align="center">
+                  <img height="110" width="110" alt="User Pic" src="default.jpg" class="img-circle img-responsive">
+                </div>
+
+
+                <div class=" col-md-9 col-lg-9 ">
+                  <table class="table table-user-information">
+                    <tbody>
+                      <tr>
+                        <td>Username: </td>
+                        <td>Tukawhut</td>
+                      </tr>
+                      <tr>
+                        <td> Favorite Resturaunts: </td>
+                        <td> <a href="http://www.sublimealehouse.com/">Sublime Alehouse</a>,<a href= "www.chinawokery.com"> China Wokery</a> ,<a href = "http://flippinpizza.com/">Flippin Pizza</a>  </td>
+                      </tr>
+                      <tr>
+
+                        <td>Major:</td>
+                        <td>Computer Science</td>
+                      </tr>
+                      <tr>
+                        <td>Joined in:</td>
+                        <td>10/20/2016</td>
+                      </tr>
+                      <tr>
+                        <td>Date of Birth</td>
+                        <td>01/24/1991</td>
+                      </tr>
+
+                         <tr>
+
+                        <td>Gender</td>
+                        <td>Male</td>
+                      </tr>
+                        <tr>
+                        <td>Lives in</td>
+                        <td>San Marcos, California</td>
+                      </tr>
+                      <tr>
+                        <td>Email</td>
+                        <td><a href="">myemail@gmail.com</a></td>
+                      </tr>
+
+
+                    </tbody>
+                  </table>
+                  <div col-md-2 col-md-offset-5>
+                  <a href="#" class="btn btn-primary">My Restaurant Ratings</a>
+                  <a href="#" class="btn btn-primary">Message</a>
+                </div>
+                </div>
+              </div>
+            </div>
+                 <div class="panel-footer">
+
+                    </div>
+
+          </div>
+        </div>
+      </div>
+    </div>
+<?php include 'footer.php'; ?>
