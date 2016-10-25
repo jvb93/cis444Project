d4b9a92e6fd02f50a5079a14a82c2e3bbb3aaa2e
RajakJamal <jamal002@cougars.csusm.edu>
2016-10-25 00:17:34 -0700

    Added 3 Pages for Website
    Add Restaurant, FAQ/ Contact, and Restaurant Profile Page has been added

---
+++ faqContact.php
@@ -0,0 +1,37 @@
+<?php include 'header.php';?>
+<div id ="main">
+
+<div id= "faqContact">
+  <div class="col-md-12">
+    <div class="row">
+        <div class="col-md-12"><div class="well"><h1>FAQ/Contact Information</h1></div></div>
+    </div>
+    <div class="row">
+        <div class="col-md-12"><div class="well">
+          <h2>Frequently Asked Question #1</h2>
+          <p>Frequently asked question answer  </br></p>
+          <h2>Frequently Asked Question #2</h2>
+          <p>Frequently asked question answer2 </p>
+          <h2>Contact at:</h2>
+          <p>Enter Email or Phone information here </br></p>
+          <form id = "suggestionForm" action = "">
+            <label>Suggestions:</label>
+            <textarea class="form-control" rows="3"></textarea>
+            <input type = "submit" value = "Submit" />
+          </form>
+        </div>
+
+  </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+</div>
+  <?php include 'footer.php'; ?>
---
+++ restaurantProfile.php
@@ -0,0 +1,51 @@
+<?php include 'header.php';?>
+<div id ="main">
+<div id= "restaurantName">
+    <div class="col-md-12">
+      <div class="row">
+          <div class="col-md-12"><div class="well"><h1>Restaurant Name</h1></div></div>
+      </div>
+      <div class="row">
+          <div class="col-md-6"><div class="well">
+            <p> Restaurant's Distance, Rating, Price, etc. </p>
+            <p> Resturant's Bio: Description, History, Deal's, Phone Numbers, Address, etc. </p>
+            <p> (Link to Restaurant's website if available) </p>
+              <label for"Comment Box">Comment:</label>
+              <textarea class="form-control" rows="3"></textarea>
+              <input type = "submit" value = "Submit" />
+            </form>
+            <div class = "comment">
+              <h6> Username1 </h6>
+              <p> I like this restaurant looooooool </P>
+            </div>
+
+          </div></div>
+          <div class="col-md-3"><div class="well">
+          <p>
+            <img src = "" alt = "'Picture of Place on Map'" />
+          </br>
+            <img src = "" alt = "'Picture of Restaurant'" />
+          </p>
+          </div></div>
+
+          <div class="col-md-3"><div class="well">
+
+            <div class = "suggestedRestaurant">
+              <p>
+                <img src = "" alt = "'Picture of Suggested Restaurant'" />
+              </p>
+            </div>
+
+            <div class = "suggestedRestaurant">
+              <p>
+                <img src = "" alt = "'Picture of Suggested 2 Restaurant'" />
+              </p>
+            </div>
+
+        </div></div>
+      </div>
+
+
+
+</div>
+<?php include 'footer.php'; ?>
