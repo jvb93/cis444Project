6f5dc1e5b2f741cce8de97356ffc54d6e85c7f36
Shane <shane.s.sharareh@gmail.com>
2016-10-24 17:22:12 -0700

    Wrote login.php and registration.php
    Let me know if anything else needs to be added to registration.php

---
+++ registration.php
@@ -0,0 +1,35 @@
+<?php include 'header.php';?>
+
+<div class=" col-md-6 col-md-offset-2" >
+        <h3 class="dark-grey">Registration</h3>
+
+        <div class="form-group col-lg-12">
+          <label>Username</label>
+          <input type="" name="" class="form-control" id="" value="">
+        </div>
+
+        <div class="form-group col-lg-6">
+          <label>Password</label>
+          <input type="password" name="" class="form-control" id="" value="">
+        </div>
+
+        <div class="form-group col-lg-6">
+          <label>Repeat Password</label>
+          <input type="password" name="" class="form-control" id="" value="">
+        </div>
+
+        <div class="form-group col-lg-6">
+          <label>Email Address</label>
+          <input type="" name="" class="form-control" id="" value="">
+        </div>
+
+        <div class="form-group col-lg-6">
+          <label>Repeat Email Address</label>
+          <input type="" name="" class="form-control" id="" value="">
+        </div>
+        <div class="form-group col-lg-6">
+          <button type="back" class="btn btn-primary">Back</button>
+        <button type="submit" class="btn btn-primary">Register</button>
+       </div>
+</div>
+<?php include 'footer.php'; ?>
