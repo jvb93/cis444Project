735c0d29595e5910dacc3093e6d2bdc942ba46fd
Shane <shane.s.sharareh@gmail.com>
2016-11-27 16:58:20 -0800

    Changed to my database name
    Feel free to change this to your guy's database name if you need to

--- phpFunctions/db_connect.php
+++ phpFunctions/db_connect.php
@@ -1,17 +1,17 @@
-<?php 
+<?php
 //$DBConnect = @mysql_connect("74.53.178.38","gomer_giants","gazelle")
-$DBConnect = @mysql_connect("localhost","example","examplePass")
+$DBConnect = @mysql_connect("localhost", "shara002", "shara002")
 	 or die("<p>The MySQL database is not available.</p>"
         . "<p>Error Code: " . mysql_errno()
         . ": " . mysql_error()) . "<p>";
 
 
 //$DBselected = @mysql_select_db("gomer_giants")
-$DBselected = @mysql_select_db("example")
+$DBselected = @mysql_select_db("shara002")
 	or die("<p> Unable to select database.</P>"
 			. "<p> Error Code: " . mysql_errno()
 			. ": " . mysql_error() . "<p>");
-			
+
 include"insertLog.php";
 
 ?>
