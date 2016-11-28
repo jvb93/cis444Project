342cdd2bd2808370aad49c9bd88da6071bc4fbe2
Shane <shane.s.sharareh@gmail.com>
2016-11-27 16:56:59 -0800

    Made some changes

--- addRestaurant.php
+++ addRestaurant.php
@@ -9,32 +9,13 @@
           <form id = "commentForm" action = "">
             <p>
             <label> Restaurant Name:
-            <input type "text" id = "restaurantName" />
-          </label>
-        </br>
-          <label> Address:
-          <input type "text" id = "restaurantAddress" />
-          </label>
-        </br>
-          <label> City:
-          <input type "text" id = "restaurantCity" />
-          </label>
-        </br>
-          <label> State:
-          <input type "text" id = "restaurantState" />
-          </label>
-        </br>
-          <label> Zip Code:
-          <input type "text" id = "restaurantZip" />
+            <input required type="text" id = "restaurantName" />
           </label>
         </br>
           <label> Website:
-          <input type "text" id = "restaurantSite" />
+          <input required type="url" id = "restaurantSite" />
           </label>
         </br>
-        <label>Bio:</label>
-        <textarea class="form-control" rows="3"></textarea>
-      </br>
         </p>
         <input type = "submit" value = "Submit" />
           </form>
