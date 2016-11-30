b52831cf0d092b5be79cfb87215c794890f53cfa
jvb93 <justin.vanbibber@me.com>
2016-11-29 19:26:24 -0800

    basic list restaurant
    Ur welcome, travis. i got the bones of this working but you'll need to
    finish it - specifically, tags and scoring.

--- index.php
+++ index.php
@@ -1,227 +1,92 @@
 <?php include 'header.php';?>
- 
+
 <div class="col-md-12">
     <div class="row">
     <div class="col-md-10">
       <h1> Local Restaurants </h1>
     </div>
     <div class="col-md-2">
-       
+
     </div>
-      
-        
+
+
     </div>
     <div class="row">
-        <div class="panel">
-            <div class="panel-heading">
-                <div class="text-center">
-                    <div class="row">
-                        <div class="col-sm-9">
-                            <h3 class="pull-left"><a href="#">Sublime Alehouse</a></h3>
+      <?php
+      $result = mysql_query("SELECT * FROM Restaurant")
+              or die("<p>Could not perform database query by device type types.</p>"
+              . "<p>Error Code " . mysql_errno()
+              . ": " . mysql_error()) . "<p>";
+
+              if(mysql_num_rows($result) > 0)
+              {
+                $dataRow = mysql_fetch_row($result);
+                do{
+
+
+                  $tags =  mysql_query("SELECT tag_value from Tag join Tag_Restaurant_Mapping on Tag_Restaurant_Mapping.tag_id = Tag.id where Tag_Restaurant_Mapping.restaurant_id = '{$dataRow[0]}'")
+                            or die("<p>Could not perform database query by device type types.</p>"
+                            . "<p>Error Code " . mysql_errno()
+                            . ": " . mysql_error()) . "<p>";
+
+                  echo("<div class='panel'>
+                        <div class='panel-heading'>
+                            <div class='text-center'>
+                                <div class='row'>
+                                    <div class='col-sm-9'>
+                                        <h3 class='pull-left'><a href='restaurant.php?id={$dataRow[0]}'>{$dataRow[1]}</a></h3>
+                                    </div>
+                                    <div class='col-sm-3'>
+                                        <h4 class='pull-right'>
+                                            <small><em>Submitted:{$dataRow[4]}</em></small>
+                                        </h4>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                        <div class="col-sm-3">
-                            <h4 class="pull-right">
-                                <small><em>Submitted: 10/23/2016</em></small>
-                            </h4>
+
+                        <div class='panel-body row'>
+                            <div class='col-md-1 voteArea'>
+                                <div class='row'>
+                                    <span class='glyphicon glyphicon-chevron-up' aria-hidden='true'></span>
+                                </div>
+                                <div class='row'>
+                                206
+                                </div>
+                                <div class='row'>
+                                    <span class='glyphicon glyphicon-chevron-down' aria-hidden='true'></span>
+                                </div>
+
+                            </div>
+                            <div class='col-md-11'>
+
+                            </div>
+
                         </div>
-                    </div>
-                </div>
-            </div>
+                        <div class='panel-footer'>");
+                        $dataRow2 = mysql_fetch_row($tags);
+                        do{
 
-            <div class="panel-body row">
-                <div class="col-md-1 voteArea">
-                    <div class="row">
-                        <span class="glyphicon glyphicon-chevron-up" aria-hidden="true"></span>
-                    </div>
-                    <div class="row">
-                    206
-                    </div>
-                    <div class="row">
-                        <span class="glyphicon glyphicon-chevron-down" aria-hidden="true"></span>
-                    </div>
 
-                </div>
-                <div class="col-md-11">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                    consequat. Duis aute irure dolor in... <a href="#">Read more</a>
-                </div>
+                            echo("<span class='label label-default'>{$dataRow2[0]}</span>");
 
-            </div>
-            <div class="panel-footer">
-                <span class="label label-default">Beer</span> <span class="label label-default">Open Late</span> <span class="label label-default">Cheap</span>
-            </div>
-        </div>
-        <div class="panel">
-            <div class="panel-heading">
-                <div class="text-center">
-                    <div class="row">
-                        <div class="col-sm-9">
-                            <h3 class="pull-left"><a href="#">Flippin' Pizza</a></h3>
-                        </div>
-                        <div class="col-sm-3">
-                            <h4 class="pull-right">
-                                <small><em>Submitted: 10/23/2016</em></small>
-                            </h4>
-                        </div>
-                    </div>
-                </div>
-            </div>
+                            $dataRow2 = mysql_fetch_row ($tags);
+                        } while ($dataRow2);
 
-            <div class="panel-body row">
-                <div class="col-md-1 voteArea">
-                    <div class="row">
-                        <span class="glyphicon glyphicon-chevron-up" aria-hidden="true"></span>
-                    </div>
-                    <div class="row">
-                    199
-                    </div>
-                    <div class="row">
-                        <span class="glyphicon glyphicon-chevron-down" aria-hidden="true"></span>
-                    </div>
 
-                </div>
-                <div class="col-md-11">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                    consequat. Duis aute irure dolor in... <a href="#">Read more</a>
-                </div>
 
-            </div>
-            <div class="panel-footer">
-                <span class="label label-default">Pizza</span> <span class="label label-default">Student Discount</span> <span class="label label-default">Cheap</span>
-            </div>
-        </div>
-        <div class="panel">
-            <div class="panel-heading">
-                <div class="text-center">
-                    <div class="row">
-                        <div class="col-sm-9">
-                            <h3 class="pull-left"><a href="#">Flippin' Pizza</a></h3>
-                        </div>
-                        <div class="col-sm-3">
-                            <h4 class="pull-right">
-                                <small><em>Submitted: 10/23/2016</em></small>
-                            </h4>
-                        </div>
-                    </div>
-                </div>
-            </div>
 
-            <div class="panel-body row">
-                <div class="col-md-1 voteArea">
-                    <div class="row">
-                        <span class="glyphicon glyphicon-chevron-up" aria-hidden="true"></span>
-                    </div>
-                    <div class="row">
-                    199
-                    </div>
-                    <div class="row">
-                        <span class="glyphicon glyphicon-chevron-down" aria-hidden="true"></span>
-                    </div>
+                        echo("</div>
+                    </div>");
 
-                </div>
-                <div class="col-md-11">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                    consequat. Duis aute irure dolor in... <a href="#">Read more</a>
-                </div>
+                    $dataRow = mysql_fetch_row ($result);
+                } while ($dataRow);
+              }
 
-            </div>
-            <div class="panel-footer">
-                <span class="label label-default">Pizza</span> <span class="label label-default">Student Discount</span> <span class="label label-default">Cheap</span>
-            </div>
-        </div>
-        <div class="panel">
-            <div class="panel-heading">
-                <div class="text-center">
-                    <div class="row">
-                        <div class="col-sm-9">
-                            <h3 class="pull-left"><a href="#">Flippin' Pizza</a></h3>
-                        </div>
-                        <div class="col-sm-3">
-                            <h4 class="pull-right">
-                                <small><em>Submitted: 10/23/2016</em></small>
-                            </h4>
-                        </div>
-                    </div>
-                </div>
-            </div>
 
-            <div class="panel-body row">
-                <div class="col-md-1 voteArea">
-                    <div class="row">
-                        <span class="glyphicon glyphicon-chevron-up" aria-hidden="true"></span>
-                    </div>
-                    <div class="row">
-                    199
-                    </div>
-                    <div class="row">
-                        <span class="glyphicon glyphicon-chevron-down" aria-hidden="true"></span>
-                    </div>
-
-                </div>
-                <div class="col-md-11">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                    consequat. Duis aute irure dolor in... <a href="#">Read more</a>
-                </div>
-
-            </div>
-            <div class="panel-footer">
-                <span class="label label-default">Pizza</span> <span class="label label-default">Student Discount</span> <span class="label label-default">Cheap</span>
-            </div>
-        </div>
-        <div class="panel">
-            <div class="panel-heading">
-                <div class="text-center">
-                    <div class="row">
-                        <div class="col-sm-9">
-                            <h3 class="pull-left"><a href="#">Flippin' Pizza</a></h3>
-                        </div>
-                        <div class="col-sm-3">
-                            <h4 class="pull-right">
-                                <small><em>Submitted: 10/23/2016</em></small>
-                            </h4>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="panel-body row">
-                <div class="col-md-1 voteArea">
-                    <div class="row">
-                        <span class="glyphicon glyphicon-chevron-up" aria-hidden="true"></span>
-                    </div>
-                    <div class="row">
-                    199
-                    </div>
-                    <div class="row">
-                        <span class="glyphicon glyphicon-chevron-down" aria-hidden="true"></span>
-                    </div>
-
-                </div>
-                <div class="col-md-11">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                    consequat. Duis aute irure dolor in... <a href="#">Read more</a>
-                </div>
-
-            </div>
-            <div class="panel-footer">
-                <span class="label label-default">Pizza</span> <span class="label label-default">Student Discount</span> <span class="label label-default">Cheap</span>
-            </div>
-        </div>
-         
-
-        
+      ?>
     </div>
-    
+
 </div>
 
 <?php include 'footer.php'; ?>
