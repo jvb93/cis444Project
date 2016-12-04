40252f6f341eb2a489a747a263dfd895deca2764
Shane <shane.s.sharareh@gmail.com>
2016-12-04 01:57:11 -0800

    Fixed login failure page
    What do you think?

--- login_Failure.php
+++ login_Failure.php
@@ -1,5 +1,24 @@
 <?php include 'header.php';?>
 
-
+<div class="row">
+        <div class= "login-form col-md-12">
+          <h1 id = "login-header"> Login </h1>
+          <h3> Oops login failed! Please try again.</h3>
+          <form id = "myForm" action="verifyLogin.php" method="post" >
+          <div class="form-group">
+            <label for="exampleInputEmail1">Username</label>
+            <input type="field" class="form-control" placeholder="Username" size ="20" name="username">
+          </div>
+          <div class="form-group">
+            <label for="exampleInputPassword1">Password</label>
+            <input type="password" class="form-control" placeholder="Password" size ="20" name="password">
+          </div>
+          <div class="form-group">
+            <button type="submit" class="btn btn-primary">Login</button>
+            <a href="signUp.php" class="btn btn-primary">Sign Up</a>
+          </div>
+        </form>
+        </div>
+      </div>
 
 <?php include 'footer.php'; ?>
